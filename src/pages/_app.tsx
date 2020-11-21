ba1ecb11564ac2289796eb9a5e37a145896412ef
Pedro Morissawa <pmorissawa@gmail.com>
2020-11-21 00:57:25 -0300

    chore: rename project, update gitignore. feat: add sw support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,10 +9,12 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter tp work with Typescript, React, NextJS and Style Components"
+          content="A simple project starter to work with Typescript, React, NextJS, Storybook and Styled Components"
         />
+        <meta name="theme-color" content="#06092B" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
